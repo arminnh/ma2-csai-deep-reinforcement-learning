f4c33bea774b9abcdbf8cf21c79d3ff6140daa1f
thierryderuyttere <thierryderuyttere@hotmail.com>
2017-12-03 20:42:41 +0100

    car can now be reset to the start position

--- self-driving-car-sim/Assets/1_SelfDrivingCar/Scripts/CarTouchesTrack.cs
+++ self-driving-car-sim/Assets/1_SelfDrivingCar/Scripts/CarTouchesTrack.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CarTouchesTrack : MonoBehaviour {
 
 	public bool touchesTrack;
-	public bool test;
+	public bool resetCar;
+	public int secondsOfFreedom = 5;
+	private Vector3 startPos;
+	private Quaternion startRotation;
+	private float startTime = 0;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+		this.startPos = transform.position;
+		this.startRotation = transform.rotation;
+
+	}	
 		
 	void Update()
 	{
 		touchesTrack = IsGrounded();
+		if (!touchesTrack && this.resetCar) {
+			if (this.startTime == 0) {
+				this.startTime = Time.time;
+			}
+		} 
+
+		if (this.startTime != 0 && (Time.time - this.startTime >= this.secondsOfFreedom) ) {
+			this.startTime = 0;
+			transform.position = this.startPos;
+			transform.rotation = this.startRotation;
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			rb.velocity = new Vector3 (0, 0, 0);
+
+		}
 	}
 
 	public bool IsGrounded()  
