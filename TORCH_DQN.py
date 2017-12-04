1501a8285076b0484d08b2df709c516fb00f6b9a
thierryderuyttere <thierryderuyttere@hotmail.com>
2017-12-04 13:33:01 +0100

    Copied DQN from https://github.com/keon/deep-q-learning/blob/master/dqn.py Also trying to port it to torch. Don't know which one will be easier

---
+++ TORCH_DQN.py
@@ -0,0 +1,105 @@
+# based on https://github.com/keon/deep-q-learning/blob/master/dqn.py
+from collections import deque
+import numpy as np
+import random
+import torch
+from torch import nn
+from torch import optim
+
+class DQNNetwork(nn.Module):
+    def __init__(self, actions_size):
+        super(DQNNetwork, self).__init__()
+
+        self.cnn1 = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(1, 4, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(4),
+            nn.Dropout2d(p=.2),
+
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(4, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
+            nn.Dropout2d(p=.2),
+
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(8, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
+            nn.Dropout2d(p=.2),
+        )
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(8 * 100 * 100, 500),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(500, 500),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(500, actions_size)
+        )
+
+
+    def forward_once(self, x):
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+class DQN:
+
+    def __init__(self, state_size, action_size, GPU=False):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+
+        # Torch
+        self.model = DQNNetwork(action_size)
+        if GPU:
+            self.model.cuda()
+
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  # returns action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax(self.model.predict(next_state)[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        # Load checkpoint and map everything to cpu in case we trained on gpu
+        checkpoint = torch.load(name, map_location=lambda storage, loc: storage)
+        self.model.load_state_dict(checkpoint["state_dict"])
+        self.optimizer.load_state_dict(checkpoint["optimizer"])
+
+        #self.model.load_weights(name)
+
+    def save(self, name):
+        state = {
+            'state_dict': self.model.net.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+        }
+        torch.save(state, name)
